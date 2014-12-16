98a359ed31fff540da68414d50c16d8adbc1a752
iampmat <patrickmatherly1994@gmail.com>
2014-12-15 23:07:10 -0500

    Added coordinate system for queue. An array stores all the pieces and piece values on load.

--- clickanddrag.js
+++ clickanddrag.js
@@ -1,17 +1,6 @@
-var correctPieces = 0;
-var listOfPieces = [undefined, undefined, undefined, undefined, undefined, undefined];
-
-/*
-TEST PIECE
-var piece = $('#card1').appendTo( '#tilePile' ).draggable( {
-        containment: '#content',
-        stack: '#tilePile div',
-        cursor: 'move',
-        revert: 'invalid'
-      } );
-piece.settingId = '#slot1';*/
-
-var savedList = [undefined, undefined, undefined, undefined, undefined, undefined];
+var listOfPieces = new Array();
+var onBoardPieces = new Array();
+var savedList = new Array();
 
 var topValues = [-1, -1, 1, -1, 1, -1];
 var rightValues = [1, -1, -1, 1, 1, -1];
@@ -20,34 +9,36 @@ var leftValues = [-1, -1, 1, 1, 1, -1];
 var sides = [ 'top', 'right', 'bottom', 'left'];
 
 var numbers = [ 1, 2, 3, 4, 5, 6 ];
-var i = 0;
-var temp;
 var pieceSize = 1;
-var slotRows = 2;
 $( init );
 
-
 function init() {
+
   // Create Queue
+  var x = 1;
+  var y = 1;
   for ( var i=0; i<6; i++ ) {
     $('<div>' + 'q' + [i] + '</div>').attr( 'id', 'queue'+numbers[i] ).appendTo( '#queuePile' ).droppable( {
         accept: '#tilePile div',
         hoverClass: 'hovered',
         drop: handleTileDrop,
     } );
+    $('#q' + [i]).data( 'y', y);
+    $('#q' + [i]).data( 'x', x);
+    x++;
   }
 
   // Create the card slots
-  var x = 0;
-  var y = 0;
+  var x = 1;
+  var y = 1;
   for ( var i=1; i<=6; i++ ) {
     $('<div>' + 'slot' + [i] + '</div>').data( 'number', i ).attr( 'id', 'slot'+[i] ).appendTo( '#tileSlots' ).droppable( {
       accept: '#tilePile div',
       hoverClass: 'hovered',
       drop: handleTileDrop,
     } ); 
-    if(x > slotRows){
-      x = 0;
+    if(x > 3){
+      x = 1;
       y++;
     }
     $('#slot' + [i]).data( 'y', y);
@@ -56,6 +47,8 @@ function init() {
   }
 
   // Create the pile of tiles
+  var x = 1;
+  var y = 1;
   for ( var i=0; i<6; i++ ) {
     if(savedList[i] != undefined){
       savedList[i].position( { of: $(savedList[i].settingId), my: 'left top', at: 'left top' } );
@@ -67,6 +60,12 @@ function init() {
         cursor: 'move',
         revert: 'invalid'
       } );
+      $( '#' + 'card' + numbers[i] ).data('queue', true);
+      $( '#' + 'card' + numbers[i] ).data( 'y', y);
+      $( '#' + 'card' + numbers[i] ).data( 'x', x);
+      listOfPieces[i] = $( '#' + 'card' + numbers[i] );
+      listOfPieces[i].id = 'card' + numbers[i];
+      x++;
     }
   }
 
@@ -123,8 +122,16 @@ function init() {
         $(id).data('ori', 1);
       } 
     }
-    //console.log($(id).data('ori'));
   });
+
+// PRINT ALL PIECES
+
+for(var i = 0; i < 6; i++){
+  console.log(listOfPieces[i]);
+  console.log('queue: ' + listOfPieces[i].data('queue'));
+  console.log('cord: ' + '(' + listOfPieces[i].data( 'x') + ', ' + listOfPieces[i].data( 'y') + ')');
+}
+
 }
 
 function rotateValues ( id ) {
@@ -138,11 +145,10 @@ function rotateValues ( id ) {
 // Piece Drop Function
 function handleTileDrop( event, ui ) {
 
-  var counter = 0, result = 0, sideResult = 0;
+  var counter = 0, sideResult = 0;
   var reject = false;
 
   // Set position of piece in droppable
-  console.log(this);
   ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
   var newPiece = ui.draggable;
   var offset = newPiece.position();
@@ -152,55 +158,46 @@ function handleTileDrop( event, ui ) {
   var settingId = '#' + $(this).attr('id'); 
   var settingNum = $(this).attr('id').split('t');
 
-  //console.log($(this).data('x'));
-  //console.log($(this).data('y'));
-
-  //console.log(settingId.data('y'));
-
-  // Already dragged peices are reset so they can be dragged again
-  for (var j = 0; j <= 6; j++) {
-    if(listOfPieces[j] == newPiece){
-      listOfPieces[j] = undefined;
-    }
-  }
-
   // Check if adjecent peices are matches. Droppables on the board are undefined.
   if(settingNum[1] >= 1){
     reject = false;
-    for (var j = 0; j <= 6; j++) {      
-      // Only array elements if it's a piece 
-      if(listOfPieces[j] != undefined){
+
+    console.log(id + ': ' + '(' + xPos + ', ' + yPos + ')');
+
+    for (var j = 0; j < 6; j++) {      
+      // Check if piece is on board 
+      if(listOfPieces[j].data('queue') == false){
         // Check piece right
-        if((listOfPieces[j].xPos == xPos + pieceSize) && (listOfPieces[j].yPos == yPos)){
+        if((listOfPieces[j].data('x') == xPos + pieceSize) && (listOfPieces[j].data('y') == yPos)){
           sideResult = newPiece.data( 'right' ) + listOfPieces[j].data( 'left' );
           if(sideResult != 0){
             reject = true;
           }  
-          console.log(' right: ' + newPiece.data( 'right' ) + (' left: ') + listOfPieces[j].data( 'left' ));
+          //console.log(' right: ' + newPiece.data( 'right' ) + (' left: ') + listOfPieces[j].data( 'left' ));
         }
         // Check piece bottom
-        if((listOfPieces[j].xPos == xPos) && (listOfPieces[j].yPos == yPos - pieceSize)){
+        if((listOfPieces[j].data('x') == xPos) && (listOfPieces[j].data('y') == yPos - pieceSize)){
           sideResult = newPiece.data( 'top' ) + listOfPieces[j].data( 'bottom' );
           if(sideResult != 0){
             reject = true;
           }
-          console.log(' top: ' + newPiece.data( 'top' ) + (' bottom: ') + listOfPieces[j].data( 'bottom' ));
+          //console.log(' top: ' + newPiece.data( 'top' ) + (' bottom: ') + listOfPieces[j].data( 'bottom' ));
         }
         // Check piece left
-        if((listOfPieces[j].xPos == xPos - pieceSize) && (listOfPieces[j].yPos == yPos)){
+        if((listOfPieces[j].data('x') == xPos - pieceSize) && (listOfPieces[j].data('y') == yPos)){
           sideResult = newPiece.data( 'left' ) + listOfPieces[j].data( 'right' );
           if(sideResult != 0){
             reject = true;
           }
-          console.log(' left: ' + newPiece.data( 'left' ) + (' right: ') + listOfPieces[j].data( 'right' ));
+          //console.log(' left: ' + newPiece.data( 'left' ) + (' right: ') + listOfPieces[j].data( 'right' ));
         }
         // Check piece top
-        if((listOfPieces[j].xPos == xPos) && (listOfPieces[j].yPos == yPos + pieceSize)){
+        if((listOfPieces[j].data('x') == xPos) && (listOfPieces[j].data('y') == yPos + pieceSize)){
           sideResult = newPiece.data( 'bottom' ) + listOfPieces[j].data( 'top' );
           if(sideResult != 0){
             reject = true;
           }
-          console.log(' bottom: ' + newPiece.data( 'bottom' ) + (' top: ') + listOfPieces[j].data( 'top' ));
+          //console.log(' bottom: ' + newPiece.data( 'bottom' ) + (' top: ') + listOfPieces[j].data( 'top' ));
         }
       }
     }
@@ -214,14 +211,14 @@ function handleTileDrop( event, ui ) {
     newPiece.position( { of: $(this), my: 'left top', at: 'left top' } );
     ui.draggable.draggable( 'option', 'revert', 'invalid' );
     $('#' + id).data('click', false);
+    $('#' + id).data('queue', false);
     // Adds the new piece to the list of all dropped
     newPiece.id = id;
-    newPiece.xPos = xPos;
-    newPiece.yPos = yPos;
-    newPiece.queue = false;
+    newPiece.data('x', $(this).data('x'));
+    newPiece.data('y', $(this).data('y'));
     newPiece.settingId = settingId;
-    for (var j = 0; j <= 6; j++) {
-      if(listOfPieces[j] == undefined){
+    for (var j = 0; j < 6; j++) {
+      if(listOfPieces[j].id == id){
         listOfPieces[j] = newPiece;
         break;
       }
@@ -231,12 +228,7 @@ function handleTileDrop( event, ui ) {
   else if(reject == 'q'){
     $('#' + id).data('click', true);
     ui.draggable.draggable( 'option', 'revert', false );
-    for (var j = 0; j <= 6; j++) {
-      if(listOfPieces[j] == ui.draggable){
-        listOfPieces[j] = undefined;
-      }
-    } 
-     ui.draggable.queue = true;
+     ui.draggable.data('queue', true);
   }
   // Revert to last position 
   else{
